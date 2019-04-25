83ba1de0d58cde5102839a627f6221fde7cf2330
zhengyizhu <zhengyizhu@sohu-inc.com>
2019-04-25 18:48:05 +0800

    create dev

---
+++ tensorflow/testfunction.py
@@ -0,0 +1,19 @@
+#encoding=utf-8
+#author:Tedrick
+#software:Pycharm
+#file:testfunction.py
+#time:2019/4/17 下午2:54
+
+# from options.test_options import TestOptions
+# config = TestOptions().parse()
+# print('this is savingpath==========',config.saving_path)
+
+# import glob
+# import os
+# pathfile = glob.glob(os.path.join('./imgs/places2_256x256/', '*.png'))
+# print(pathfile)
+
+# filelitst = open('testfile.txt', 'rt').read().splitlines()
+# print(type(filelitst),filelitst)
+
+
